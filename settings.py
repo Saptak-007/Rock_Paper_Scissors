05cf82fa218d22c51f1ba67b99fecdb672a26130
saptak <saptakbhattacharjee501@gmail.com>
2026-09-13 11:08:20 +0530

    moved font from sprites.py to settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,7 @@
-SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+import pygame
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 600
+
+# Load fonts
+font = pygame.font.Font('assets/fonts/font.ttf', 50)
+font_for_choice = pygame.font.Font('assets/fonts/font.ttf', 40)
--- sprites.py
+++ sprites.py
@@ -1,21 +1,15 @@
 import pygame
 from settings import *
-pygame.init()
 
-# Load sprites
-
+# Load sprites:
 # Load background
 bg = pygame.transform.scale(pygame.image.load("assets/sprites/background.jpeg"), (SCREEN_WIDTH, SCREEN_HEIGHT))
 bg_rect = bg.get_rect(topleft=(0, 0))
 
 # Load sprites of rock, paper, and scissors
-RPS_HEIGHT, RPS_WIDTH = 150, 150
+RPS_WIDTH, RPS_HEIGHT = 150, 150
 
 rock = pygame.transform.scale(pygame.image.load("assets/sprites/rock.png"), (RPS_HEIGHT, RPS_WIDTH))
 paper = pygame.transform.scale(pygame.image.load("assets/sprites/paper.png"), (RPS_HEIGHT, RPS_WIDTH))
 scissors = pygame.transform.scale(pygame.image.load("assets/sprites/scissors.png"), (RPS_HEIGHT, RPS_WIDTH))
 icon = pygame.image.load("assets/sprites/icon.jpg")
-
-# Load fonts
-font = pygame.font.Font('assets/fonts/font.ttf', 50)
-font_for_choice = pygame.font.Font('assets/fonts/font.ttf', 40)
